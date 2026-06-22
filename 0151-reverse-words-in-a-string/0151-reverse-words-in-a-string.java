d7f60e4d20152ee469f6d051902a160697392afe
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-06-22 20:29:27 +0530

    Time: 5 ms (85.94%), Space: 43.7 MB (96.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,13 +1,54 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
+        char[] arr = s.toCharArray();
+        reverse(arr, 0, arr.length - 1);
+        reverseWords(arr);
+        return cleanSpaces(arr);
+    }
+    
+    private void reverse(char[] arr, int start, int end){
+        while(start<end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    private void reverseWords(char[] arr){
+        int start = 0, n = arr.length;
+        for(int i=0;i<n;i++){
+             if(arr[i] == ' '){
+                reverse(arr, start, i - 1);
+                start = i + 1;
+            }
+            else if(i == n - 1){
+                reverse(arr, start, i);
+            }
+        }
+    }
+
+    private String cleanSpaces(char[] arr) {
         StringBuilder sb = new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            sb.append(words[i]);
-            if(i!=0) {
-                sb.append(" ");
+        int n = arr.length;
+        int i=0;
+        while(i<n){
+            while(i<n && arr[i]==' '){
+                i++;
+            }
+            while(i<n && arr[i]!=' '){
+                sb.append(arr[i++]);
+            }
+            while(i<n && arr[i]==' '){
+                i++;
+            }
+            if(i<n){
+                sb.append(' ');
             }
         }
         return sb.toString();
     }
+
 }
