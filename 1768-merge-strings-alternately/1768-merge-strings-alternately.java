f31cd6bbc28a101fa2868b0ef7e1393f714da1c9
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-06-22 18:22:53 +0530

    Time: 1 ms (95.73%), Space: 43.2 MB (48.32%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0, j=0;
+        StringBuilder ans = new StringBuilder();
+        while(i<word1.length() && j<word2.length()) {
+            ans = ans.append(word1.charAt(i++));
+            ans = ans.append(word2.charAt(j++));
+        }
+        if(i<word1.length())
+            ans = ans.append(word1.substring(i));
+        if(j<word2.length())
+            ans = ans.append(word2.substring(j));
+        return ans.toString();
+    }
+}
