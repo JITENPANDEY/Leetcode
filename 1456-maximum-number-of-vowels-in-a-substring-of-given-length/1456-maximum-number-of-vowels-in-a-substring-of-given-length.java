fe8ae92857cdee6205a0a8bf6f4d4d942d7014fc
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-06-24 23:53:52 +0530

    Time: 23 ms (16.87%), Space: 46.3 MB (79.36%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,38 +1,26 @@
 class Solution {
-    // 1. Move the lookup array outside to avoid constant heap allocation
-    private static final boolean[] isVowel = new boolean[128];
-    
-    // 2. Initialize it ONCE when the class loads
-    static {
-        isVowel['a'] = true;
-        isVowel['e'] = true;
-        isVowel['i'] = true;
-        isVowel['o'] = true;
-        isVowel['u'] = true;
-    }
-
     public int maxVowels(String s, int k) {
-        // 3. Avoid toCharArray() as it copies the entire string into a new memory array
-        int len = s.length();
-        int count = 0;
-        
-        // Count vowels in the first window
-        for (int i = 0; i < k; i++) {
-            if (isVowel[s.charAt(i)]) count++;
-        }
-        
-        int maxCount = count;
-        
-        // Sliding window
-        for (int i = k; i < len; i++) {
-            if (isVowel[s.charAt(i)]) count++;     // Add incoming character
-            if (isVowel[s.charAt(i - k)]) count--; // Remove outgoing character
-            
-            if (count > maxCount) {
-                maxCount = count;
+        int cnt = 0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s.charAt(i))){
+                cnt++;
             }
         }
+        int maxCnt = cnt;
         
-        return maxCount;
+        for(int i = k;i<s.length();i++){
+            if(isVowel(s.charAt(i))) {
+                cnt++;
+            }
+            if(isVowel(s.charAt(i-k))){
+                cnt--;
+            }
+            maxCnt = Math.max(maxCnt, cnt);
+            
+        }
+        return maxCnt;
+    }
+    private boolean isVowel(char c){
+        return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
