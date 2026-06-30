3f3e32e609f4afb9ee5e2e1784cab03e057ad4d2
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-06-30 22:35:51 +0530

    Time: 21 ms (10.14%), Space: 50.2 MB (18.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,13 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> group = new HashMap<>();
-        for(String s : strs){
-            char[] keyArray = s.toCharArray();
-            Arrays.sort(keyArray);
+        // #1. With Sorting O(N*KLogK)
+        // Map<String, List<String>> group = new HashMap<>();
+        // for(String s : strs){
+        //     char[] keyArray = s.toCharArray();
+        //     Arrays.sort(keyArray);
 
-            String key = String.valueOf(keyArray);
-            group.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        //     String key = String.valueOf(keyArray);
+        //     group.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        // }
+        // return new ArrayList<>(group.values());
+
+        // #2. Without Sorting O(N*K)
+        Map<String, List<String>> group = new HashMap<>();
+        for(String s : strs) {
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+            for(int cnt : count){
+                key.append('#').append(cnt);
+            }
+            group.computeIfAbsent(key.toString(), k-> new ArrayList<String>()).add(s);
         }
+        
         return new ArrayList<>(group.values());
     }
 
