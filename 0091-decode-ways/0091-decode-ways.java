24ac968c99e38071820f74ac72ddc27df483a4c8
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-07-02 21:54:35 +0530

    Time: 0 ms (100%), Space: 42.8 MB (93.1%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] memo = new int[s.length()];
+        Arrays.fill(memo, -1);
+        return decodeWays(s, 0, memo);
+    }
+
+    private int decodeWays(String s, int index, int[] memo) {
+        // agar string khatam to return kar do valid string count 1
+        if(index == s.length()){
+            return 1;
+        }
+
+        // current index value '0' aa gya it means invalid iska na to single digit bana skte na hi 2 digit bcoz leading zeros invalid hai
+        if(s.charAt(index)=='0'){
+            return 0;
+        }
+
+        //pehle check kar lo memo table pe value pada hai to recursive call mat karo
+        if(memo[index]!=-1){
+            return memo[index];
+        }
+
+        // Take one digit and process next pass
+        int ways = decodeWays(s, index+1, memo);
+
+        //Take 2 digit and process next pass but yeha check karna valid number hone chahiye 10~26 tak
+        if(index+1<s.length()){
+            int twoDigit = (s.charAt(index)-'0') * 10 + (s.charAt(index+1) - '0');
+            if(10<= twoDigit && twoDigit<=26){
+                ways += decodeWays(s, index+2, memo);
+            }
+        }
+
+        //memo me store kar lo
+        memo[index] = ways;
+        // jab left and right dono ke ways aa jaye to return kar do
+        return ways;
+    }
+}
