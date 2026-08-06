3c651660633595688747960436a2990cef69d72f
Jiten Pandey <49372019+JITENPANDEY@users.noreply.github.com>
2026-08-06 11:58:58 +0530

    Time: 2 ms (98.8%), Space: 43.3 MB (27.45%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int secondsToRemoveOccurrences(String s) {
+        int zero = 0;
+        int time = 0;
+        for(char c : s.toCharArray()) {
+            if(c == '0') {
+                zero++;
+            } else if(zero > 0) {
+                time = Math.max(time+1, zero);
+            }
+        }
+        return time;
+    }
+}
